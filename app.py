3927261fcedef6912e63eaa8b4dbddf68634437d
Aaron Boyle <aaronboyle@aarons-mbp.home>
2023-12-28 21:11:18 +0000

    adds delivery date to the add_order app.route method

--- app.py
+++ app.py
@@ -86,6 +86,7 @@ def add_order():
     product_quantity = request.form.get('product_quantity')
     order_date = request.form.get('order_date')
     shipping_date = request.form.get('shipping_date')
+    delivery_date = request.form['delivery_date']
     
     # Create a session to interact with the database
     session = Session()
