c7fb38e228572575d0eeafca443f552393471c0f
Maya Iuga <maya_iuga@yahoo.ro>
2024-08-07 12:57:03 +0100

    updates app code to use key vault secrets

--- app.py
+++ app.py
@@ -9,16 +9,29 @@ import os
 # Initialise Flask App
 app = Flask(__name__)
 
-# Database connection using environment variables
-server = os.getenv('DB_SERVER')
-database = os.getenv('DB_DATABASE')
-username = os.getenv('DB_USERNAME')
-password = os.getenv('DB_PASSWORD')
-driver= '{ODBC Driver 18 for SQL Server}'
+# database connection 
+# Replace these values with your Key Vault details
+key_vault_url = "https://keyvault-6e910da7.vault.azure.net/"
 
-# Ensure all required variables are set
-if not all([server, database, username, password, driver]):
-    raise ValueError("Database configuration is incomplete!")
+
+# Set up Azure Key Vault client
+#credential = DefaultAzureCredential(exclude_environment_credential=True)
+credential = ManagedIdentityCredential()
+secret_client = SecretClient(vault_url=key_vault_url, credential=credential)
+
+
+server_name_secret = secret_client.get_secret("5076a581-server-name-secret")
+server_username_secret = secret_client.get_secret("5076a581-server-username-secret")
+server_password_secret = secret_client.get_secret("5076a581-server-password-secret")
+database_name_secret = secret_client.get_secret("5076a581-database-name-secret")
+
+
+# Access the secret values
+server = server_name_secret.value
+username = server_username_secret.value
+password = server_password_secret.value
+database = database_name_secret.value
+driver= '{ODBC Driver 18 for SQL Server}'
 
 # Create the connection string
 connection_string=f'Driver={driver};\
